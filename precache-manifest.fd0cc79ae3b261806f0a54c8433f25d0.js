55f1c74fac57842d3a0f70c66650661f4ea2ffef
Lucas Reyna <lucas.reyna143@gmail.com>
2022-04-25 13:59:59 -0700

    Updates

---
+++ precache-manifest.fd0cc79ae3b261806f0a54c8433f25d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab8e4865e0ef7b202a3",
+    "url": "/GitFit-App/static/css/main.e798c8c2.chunk.css"
+  },
+  {
+    "revision": "bab8e4865e0ef7b202a3",
+    "url": "/GitFit-App/static/js/main.bab8e486.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/GitFit-App/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "d8f46372a7a370b8fef6",
+    "url": "/GitFit-App/static/js/2.d8f46372.chunk.js"
+  },
+  {
+    "revision": "554157837e496cca7d59",
+    "url": "/GitFit-App/static/js/runtime~main.55415783.js"
+  },
+  {
+    "revision": "2e6e928c2ad474ac2d6f45d97f2bce90",
+    "url": "/GitFit-App/static/media/dumbell.2e6e928c.svg"
+  },
+  {
+    "revision": "b535b347aa421c950b118a69e4198652",
+    "url": "/GitFit-App/index.html"
+  }
+];
