f3e1ee9d0d29824efc6eae9bb9e9c4e832bf2500
LukeSandsor <LukeSandsor@users.noreply.github.com>
2022-04-27 20:11:50 +0000

    deploy: 7bebfc16d83da52446ed641870e72b0bda4c8aea

---
+++ precache-manifest.f0462b6723e91c74cb6c4012e186d602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "11443b4d9806af9c6693",
+    "url": "/static/css/main.713531c3.chunk.css"
+  },
+  {
+    "revision": "11443b4d9806af9c6693",
+    "url": "/static/js/main.1778269a.chunk.js"
+  },
+  {
+    "revision": "f7ad848f3f6490dc6991",
+    "url": "/static/js/runtime~main.6a4d41bb.js"
+  },
+  {
+    "revision": "a2380d939e3837b7bf10",
+    "url": "/static/js/2.87848f2a.chunk.js"
+  },
+  {
+    "revision": "9dc639999e908e298750",
+    "url": "/static/js/3.1090266c.chunk.js"
+  },
+  {
+    "revision": "2e6e928c2ad474ac2d6f45d97f2bce90",
+    "url": "/static/media/dumbell.2e6e928c.svg"
+  },
+  {
+    "revision": "4390348839b75c9248f2bf90b2a70d9c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/GitFit-App/precache-manifest.ba12fd264890ec73466eee05321e3f53.js"
+  "/precache-manifest.f0462b6723e91c74cb6c4012e186d602.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/GitFit-App/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
