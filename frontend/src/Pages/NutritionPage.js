afe695e44243c13167c0850cfc40cd66971ffc59
jmarenin <jmarenin@calpoly.edu>
2022-04-30 14:26:09 -0700

    Basic Bar Chart

--- frontend/src/Pages/NutritionPage.js
+++ frontend/src/Pages/NutritionPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../NavBar';
 import DoughnutChart from '../DoughnutChart';
+import BarChart from '../BarChart';
 import './NutritionPage.css'
 
 function NutritionPage() {
@@ -11,6 +12,9 @@ function NutritionPage() {
             <h1>Nutrition Page</h1>
             <div className='doughnut'>
                 <DoughnutChart />
+            </div>
+            <div>
+                <BarChart />
             </div>
         </div>
     )
