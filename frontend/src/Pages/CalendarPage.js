581b91acff84fb97f45824bbf79384b501b06dde
Lucas Reyna <lucas.reyna143@gmail.com>
2022-04-30 12:36:20 -0700

    after painfully implementing contexts into the CalendarPage.js, I am happy to announce that the calendar will now load info on a specific day. The code looks kinda like garbage, but it works for now. I just hope that there won't be many slow downs when incorporating the backend. We'll have to see about caching, maybe.

--- frontend/src/Pages/CalendarPage.js
+++ frontend/src/Pages/CalendarPage.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Calendar from '../CalendarObject/Calendar';
 import NavBar from '../NavBar';
 import './CalendarPage.css';
+import CalendarDateInfoContext from '../context/calendar-date.context';
 
 function CalendarPage()
 {
+  const [dailyInfo, setDailyInfo] = useState({
+    calLost: "",
+    calCons: "",
+    weight: "",
+    numWork: "",
+    numStep: "",
+    mood: ""
+  });
+
+  const updateDay = (dailyInfo) => setDailyInfo(dailyInfo);
+
   return (
     <div>
       <NavBar />
       <h3>GitFit: Calendar</h3>
       <div id="page-contents">
-        <Calendar />
+        <CalendarDateInfoContext.Provider value={{dailyInfo, updateDay}}>
+          <Calendar />
+        </CalendarDateInfoContext.Provider>
         <div id="day-info-block">
           <div id="day-title">Select a date to see your fitness information</div>
-          <h3 style={{"margin-bottom": "0"}}>Daily Breakdown</h3>
+          <h3 style={{marginBottom: "0"}}>Daily Breakdown</h3>
           <div id="daily-breakdown">
-            <p>Calories Lost: </p>
-            <p>Calories Consumed: </p>
-            <p>Weight: </p><br></br>
-            <p># of Workouts: </p>
-            <p># of Steps Taken: </p>
-            <p>Mood for the Day: </p>
+            <p>Calories Lost: {dailyInfo.calLost}</p>
+            <p>Calories Consumed: {dailyInfo.calCons}</p>
+            <p>Weight: {dailyInfo.weight}</p><br></br>
+            <p># of Workouts: {dailyInfo.numWork}</p>
+            <p># of Steps Taken: {dailyInfo.numStep}</p>
+            <p>Mood for the Day: {dailyInfo.mood}</p>
           </div>
         </div>
       </div>
