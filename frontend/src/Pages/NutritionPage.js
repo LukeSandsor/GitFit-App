f4eb3a4a45751002bd8375794cb0d829d30538cb
jmarenin <jmarenin@calpoly.edu>
2022-05-02 13:56:29 -0700

    Table containing nutrition statistics

--- frontend/src/Pages/NutritionPage.js
+++ frontend/src/Pages/NutritionPage.js
@@ -18,6 +18,42 @@ function NutritionPage() {
                     <BarChart />
                 </div>
             </div>
+            <div className='macro-table-container'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Macro Nutrient</th>
+                            <th>Actual Intake</th>
+                            <th>Target Intake</th>
+                            <th>Actual Percentage</th>
+                            <th>Target Percentage</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Protein</td>
+                            <td>120 g</td>
+                            <td>180 g</td>
+                            <td>40 %</td>
+                            <td>50 %</td>
+                        </tr>
+                        <tr>
+                            <td>Carbohydrates</td> 
+                            <td>200 g</td>
+                            <td>220 g</td>
+                            <td>30 %</td>
+                            <td>50 %</td>
+                        </tr>
+                        <tr>
+                        <td>Fats</td> 
+                            <td>60 g</td>
+                            <td>80 g</td>
+                            <td>10 %</td>
+                            <td>20 %</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
