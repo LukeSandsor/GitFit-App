e3d27bbfe1e3f1ac7b83d310bf15609c4fdedf9b
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-05-05 19:15:54 -0700

    Configured Register Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
@@ -8,7 +9,6 @@ import SummaryPage from './Pages/SummaryPage';
 import CalendarPage from './Pages/CalendarPage';
 import WeightsPage from './Pages/WeightsPage';
 import BMIPage from './Pages/BMIPage';
-import {Route, Routes} from 'react-router-dom';
 import ErrorPage from './Pages/ErrorPage';
 
 function App() {
