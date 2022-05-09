5cd5766dbd3a0d13a54a64506118a3044b6fdaee
jmarenin <76976999+jmarenin@users.noreply.github.com>
2022-05-08 23:19:52 -0700

    Added drop down menu with very limited food selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import SummaryPage from './Pages/SummaryPage';
 import CalendarPage from './Pages/CalendarPage';
 import WeightsPage from './Pages/WeightsPage';
 import NutritionPage from './Pages/NutritionPage';
-import {Route, Routes} from 'react-router-dom';
 import ErrorPage from './Pages/ErrorPage';
 
 function App() {
