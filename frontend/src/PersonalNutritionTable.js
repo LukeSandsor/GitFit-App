cbd27ba268a8680e7b403ec7d5d828e3ee35d8ee
jmarenin <76976999+jmarenin@users.noreply.github.com>
2022-05-22 16:52:18 -0700

    table properly updated with percentage breakdowns

--- frontend/src/PersonalNutritionTable.js
+++ frontend/src/PersonalNutritionTable.js
@@ -6,6 +6,11 @@ function PersonalNutritionTable(props) {
     const carbs = `${Math.round(props.userFoodInfo.carbs * 100) / 100} g`;
     const fats = `${Math.round(props.userFoodInfo.fats * 100) / 100} g`;
     
+    const total = props.userFoodInfo.protein + props.userFoodInfo.carbs + props.userFoodInfo.fats;
+    const proteinPercentage = `${Math.round(props.userFoodInfo.protein / total * 10000) / 100} %`;
+    const carbPercentage = `${Math.round(props.userFoodInfo.carbs / total * 10000) / 100} %`;
+    const fatPercentage = `${Math.round(props.userFoodInfo.fats / total * 10000) / 100} %`;
+
     return (
         <table className='macro-table'>
                     <thead>
@@ -22,21 +27,21 @@ function PersonalNutritionTable(props) {
                             <td className='table-data'>Protein</td>
                             <td className='table-data'>{protein}</td>
                             <td className='table-data'>180 g</td>
-                            <td className='table-data'>40 %</td>
+                            <td className='table-data'>{proteinPercentage}</td>
                             <td className='table-data'>50 %</td>
                         </tr>
                         <tr>
                             <td className='table-data'>Carbohydrates</td> 
                             <td className='table-data'>{carbs}</td>
                             <td className='table-data'>220 g</td>
-                            <td className='table-data'>30 %</td>
+                            <td className='table-data'>{carbPercentage}</td>
                             <td className='table-data'>50 %</td>
                         </tr>
                         <tr>
                             <td className='table-data'>Fats</td> 
                             <td className='table-data'>{fats}</td>
                             <td className='table-data'>80 g</td>
-                            <td className='table-data'>10 %</td>
+                            <td className='table-data'>{fatPercentage}</td>
                             <td className='table-data'>20 %</td>
                         </tr>
                     </tbody>
