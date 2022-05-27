5a3b05c0a9fdb00909a946a266a4f5f61cd69807
LukeSandsor <63250619+LukeSandsor@users.noreply.github.com>
2022-05-27 12:26:33 -0700

    Hot fixes

--- frontend/src/CalendarObjects/MoodBarGraph.js
+++ frontend/src/CalendarObjects/MoodBarGraph.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
-import React from 'react';
 
 const colorsForEmoji = {'😡': 'salmon', '😢': 'lightblue', '🤒': 'green', '😐': 'gray', '🙂': 'lightyellow', '🤩': 'pink'};
 
