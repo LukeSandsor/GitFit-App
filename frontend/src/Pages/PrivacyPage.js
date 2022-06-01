2d1e48308b08b3bf895ddcec246f7384f1738488
Brett Gowling <bgowling@calpoly.edu>
2022-05-31 22:27:02 -0700

    Added description to about page
    Added a welcome message and a mission statement.

--- frontend/src/Pages/PrivacyPage.js
+++ frontend/src/Pages/PrivacyPage.js
@@ -21,7 +21,7 @@ function PrivacyPage() {
                         <h1>Privacy Policy of GitFit</h1>
                         <p>GitFit operates the www.gitfit.me website, which provides the personalized fitness-tracking service.</p>
                         <p>This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the GitFit website.</p>
-                        <p>If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. Our Privacy Policy was created with the help of a Privacy Policy Generator.</p>
+                        <p>If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
                         <h2>Information Collection and Use</h2>
                         <p>For a better experience while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and postal address. The information that we collect will be used to contact or identify you.</p>
                         <h2>Log Data</h2>
